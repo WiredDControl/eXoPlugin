1618c055226701f1d501ef0901709ecb5ad2fe18
WiredDControl <wireddcontrol@gmail.com>
2023-08-05 00:56:00 +0200

    - Install-flag works - splash screen works - submenu for english extras works - submenus for lp extras doesn't work

--- eXoPlugin/eXoPlugin.cs
+++ eXoPlugin/eXoPlugin.cs
@@ -2,21 +2,153 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Forms;
 using Unbroken.LaunchBox.Plugins;
 using Unbroken.LaunchBox.Plugins.Data;
 
 namespace eXoPlugin
 {
-    public class eXoPlugin : IGameMultiMenuItemPlugin
+
+    public class eXoPluginEvent : ISystemEventsPlugin
     {
+
+        public void OnEventRaised(string eventType)
+        {
+            // wait for Launchbox to be loaded completely
+            if (eventType == SystemEventTypes.LaunchBoxStartupCompleted
+                || eventType == SystemEventTypes.BigBoxStartupCompleted)
+            {
+                // Check for the FAQ-file				
+                string checkFile = "";  // we need the root path + /eXo/util
+                var claunchBoxPath = (new FileInfo(AppDomain.CurrentDomain.BaseDirectory)).Directory.Parent.FullName;
+                checkFile = Path.Combine(claunchBoxPath, "eXo", "util", "FAQread.txt");
+
+                if (File.Exists(checkFile) == false)
+                {
+                    //show the startup dialog box
+                    string textFile = Path.Combine(claunchBoxPath, "eXo", "util", "eXoplugin.txt");
+                    string message = "";
+                    if (File.Exists(textFile) == false)
+                    {
+                        message = "Thank you for downloading one of the eXo projects. This message will be displayed every time on start-up until you have opened the FAQ web page (select >Yes<).\r\n\r\nPlease note:\r\nSome games do not have cover art, as they were not actually released in boxes. Registered Launchbox users may switch to using the title screen shots instead.\r\nAlso, all eXo projects can be merged. An instructional video along with step by step instructions can be found on the project's FAQ page.\r\n\r\nWould you like to view it now?";
+                    }
+                    else
+                    {
+                        message = System.IO.File.ReadAllText(@textFile);
+                    }
+
+                    //new SplashScreen().Show();
+
+                    DialogResult d;
+                    d = System.Windows.Forms.MessageBox.Show(
+                        message,
+                        "FAQ",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information,
+                        MessageBoxDefaultButton.Button1,
+                        0,
+                        "https://www.retro-exo.com/FAQ.html",
+                        "");
+
+                    // if the user answers the dialog box with "yes":
+                    if (d == DialogResult.Yes)
+                    {
+                        // write the check file
+                        try
+                        {
+                            using (StreamWriter sw = File.CreateText(checkFile))
+                            {
+                                sw.WriteLine("Congrats, you've read the FAQ! ");
+                            }
+                        }
+                        catch (Exception Ex)
+                        {
+                            Console.WriteLine(Ex.ToString());
+                        }
+
+                        // open the FAQ-Website
+                        var psi = new ProcessStartInfo
+                        {
+                            FileName = "https://www.retro-exo.com/FAQ.html",
+                            UseShellExecute = true
+                        };
+                        Process.Start(psi);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public class eXoPlugin : IGameMultiMenuItemPlugin, IGameLaunchingPlugin, IGameConfiguringPlugin
+    {
+
+        public IGame GameLaunched { get; set; }     // property for the game infos
+
+        private void UpdateInstallFlag(IGame game)
+        {
+            string checkPath = ""; // initialise the variable
+
+            var launchBoxPath = (new FileInfo(AppDomain.CurrentDomain.BaseDirectory)).Directory.Parent.FullName;
+            string completeGamePath = Path.GetDirectoryName(game.ApplicationPath).ToLower(new CultureInfo("en-US", false));
+
+            if (completeGamePath.Contains("exo"))
+            {
+                string[] folders = completeGamePath.Split(Path.DirectorySeparatorChar);                             // split into folder names
+                int folderCount = folders.Length;																	// get the number of folders
+                checkPath = Path.Combine(launchBoxPath, "eXo", folders[folderCount - 3], folders[folderCount - 1]);	 	// add gamefolder to checkPath
+                game.Installed = Directory.Exists(checkPath);           											// if path exists, set Installed = true, otherwise set Installed = false
+                PluginHelper.LaunchBoxMainViewModel?.RefreshData();     											// refresh the LB view
+            }
+
+        }
+
+        public void OnAfterGameConfigurationOpens(IGame game)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnAfterGameLaunched(IGame game, IAdditionalApplication app, IEmulator emulator)
+        {
+            GameLaunched = game;
+        }
+
+        public void OnBeforeGameConfigurationOpens(IGame game)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnBeforeGameLaunching(IGame game, IAdditionalApplication app, IEmulator emulator)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnGameConfigurationExited(IGame game)
+        {
+            UpdateInstallFlag(game);
+        }
+
+        public void OnGameExited()
+        {
+            //throw new NotImplementedException();
+            //PluginHelper.DataManager.ForceReload();
+            //PluginHelper.DataManager.ReloadIfNeeded();
+
+            var game = GameLaunched;    // get the property
+            UpdateInstallFlag(game);    // do the magic
+            GameLaunched = null;        // reset property
+        }
+
         public IEnumerable<IGameMenuItem> GetMenuItems(params IGame[] selectedGames)
         {
 
             var basePath = new FileInfo(AppDomain.CurrentDomain.BaseDirectory).Directory.Parent.FullName;   // Launchbox path
             string gamePath = "";
+
 
             // make sure it's a game from an eXo-project
             if (!Path.GetDirectoryName(selectedGames[0].ApplicationPath).Contains("eXo\\eXo"))
@@ -56,7 +188,6 @@ namespace eXoPlugin
 
             var returnList = new List<GameMenuItem>();
 
-
             //if there is an alternate launcher for the game, add it
             if (File.Exists(Path.Combine(gamePath, "Extras", "Alternate Launcher.bat")))
             {
@@ -78,9 +209,8 @@ namespace eXoPlugin
                 returnList.Add(new GameMenuItem("English Extras", enitemList, "english-flag.png"));
             }
 
-
-
-
+            
+ 
             // Add the language pack extra files as menue entries
             String language = "";
 
@@ -90,8 +220,21 @@ namespace eXoPlugin
                 //get language from .LANG filename, e.g. GERMAN.LANG --> german
                 language = Path.GetFileNameWithoutExtension(languagefile.ToString().ToLower());
 
-            }
+                // Add the language pack extras files as menu entries
+                var lpitemList = new List<IGameMenuItem>();
+                var lpgamePath = gamePath.Replace("!dos\\", "!dos\\!" + language + "\\");
 
+                foreach (var filename in new DirectoryInfo(Path.Combine(lpgamePath, "Extras")).GetFiles("*.*").Where(x => x.Name != "Alternate Launcher.bat"))
+                {
+                    lpitemList.Add(new GameMenuItem(selectedGames[0], filename.ToString()));
+                }
+
+                if (lpitemList.Count > 0)
+                {
+                    returnList.Add(new GameMenuItem("German Extras", lpitemList, "german-flag.png"));
+                }
+
+            }
 
             return returnList;
 
